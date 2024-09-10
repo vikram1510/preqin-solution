f6ecad8cbdd10146c9ad20f1a56eb6b88dac65b6
Vikram Bageja <vikram.bageja@gmail.com>
2024-09-10 23:13:55 +0100

    Add investor name to commitments table

--- frontend/src/routes/commitments/Commitments.tsx
+++ frontend/src/routes/commitments/Commitments.tsx
@@ -2,12 +2,14 @@ import { useLocation } from "react-router-dom"
 import { useFetch } from "../../hooks/useFetch"
 import { COMMITTMENTS_API_URL } from "../../constants"
 import { Commitment } from "./types"
+import { investorNameFromSearchParams } from "./utils/investorNameFromSearchParams"
 
 export const Commitments = () => {
   const { search } = useLocation()
   const { data, loading } = useFetch<Commitment[]>(
     `${COMMITTMENTS_API_URL}${search}`
   )
+  const investorName = investorNameFromSearchParams(search)
 
   if (loading) return <p>Loading...</p>
 
@@ -15,7 +17,7 @@ export const Commitments = () => {
 
   return (
     <div className="centered-container">
-      <h1>Commitments</h1>
+      <h1>{`Commitments for Investor - ${investorName}`}</h1>
       <table border={1}>
         <thead>
           <tr>
@@ -26,7 +28,7 @@ export const Commitments = () => {
         </thead>
         <tbody>
           {data.map((commitment) => (
-            <tr>
+            <tr key={commitment.id}>
               <th>{commitment.id}</th>
               <th>{commitment.asset_class}</th>
               <th>{commitment.amount}</th>
