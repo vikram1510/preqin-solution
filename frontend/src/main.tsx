b470fa1d83595fe417b68c652a3776b57338722d
Vikram Bageja <vikram.bageja@gmail.com>
2024-09-10 15:45:33 +0100

    Add table with css

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
-import "./index.css"
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import { Investors } from "./routes/investors/Investors.tsx"
 import { Commitments } from "./routes/commitments/Commitments.tsx"
+
+import "./index.css"
+import "./App.css"
 
 const router = createBrowserRouter([
   {
--- frontend/src/routes/investors/Investors.tsx
+++ frontend/src/routes/investors/Investors.tsx
@@ -1,1 +1,29 @@
-export const Investors = () => <div>Investors</div>
+export const Investors = () => {
+  return (
+    <div className="centered-container">
+      <h1>Investors</h1>
+      <table border={1}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Investor Name</th>
+            <th>Investor Type</th>
+            <th>Investor Country</th>
+            <th>Date Added</th>
+            <th>Total Commitment</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th>1</th>
+            <th>Vikram Bageja</th>
+            <th>fund manager</th>
+            <th>Singapore</th>
+            <th>2000-07-06</th>
+            <th>£4050</th>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
+}
